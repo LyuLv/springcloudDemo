d5c9febdbd090b3a5bb7dfda2762a549cb6b08eb
lv <944099767@qq.com>
2020-11-16 14:43:02 +0800

    测试目录

---
+++ excute-easy/src/test/java/com/lyu/excute/easy/ExcuteEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyu.excute.easy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcuteEasyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
